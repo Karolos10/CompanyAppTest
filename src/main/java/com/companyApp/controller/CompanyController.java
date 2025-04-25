de7e32b4eec8c970720da612f345fb451228b5c6
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-25 16:06:31 -0500

    Creation methods delete and details company

--- src/main/java/com/companyApp/controller/CompanyController.java
+++ src/main/java/com/companyApp/controller/CompanyController.java
@@ -5,7 +5,9 @@ import com.companyApp.repository.CompanyRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/company")
@@ -39,5 +41,24 @@ public class CompanyController {
             return companyRespository.save(existingCompany);
         }
         return null;
+    }
+
+    @DeleteMapping("/delete/{code}")
+    public void deleteCompany(@PathVariable Long code) {
+        companyRespository.deleteById(code);
+    }
+
+    @GetMapping("/details/{codeCompany}")
+    public Map<String, Object> getCompanyDetails(@PathVariable String codeCompany) {
+        Company company = companyRespository.findByCodeCompany(codeCompany);
+        Map<String, Object> response = new HashMap<>();
+        if (company != null) {
+            return Map.of(
+                    "name", company.getNameCompany(),
+                    "code", company.getCodeCompany(),
+                    "description", company.getDescripcionCompany()
+            );
+        }
+        return Map.of("error", "Company not found");
     }
 }
